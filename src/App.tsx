aeb59e0115bd5b6a6f3828eb71f84e744c07ae75
lucasbnunes <lucasbnunes23@gmail.com>
2023-08-12 21:12:10 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme'
 import { GlobalStyle } from './styles/global'
+import { Home } from './pages/Home'
+import { Layout } from './components/layout'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Layout>
+        <Home />
+      </Layout>
     </ThemeProvider>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export function Home() {
+  return <div>
+    <h1>Contatos</h1>
+  </div>
+}
