e3998f9cd7494e7c949de62bc938c893dceeeb8b
lucasbnunes <lucasbnunes23@gmail.com>
2023-08-15 20:55:53 -0300

    fix: prevent refetch on focus to persist data

--- src/features/contacts/useContacts.ts
+++ src/features/contacts/useContacts.ts
@@ -2,9 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { client } from '../../api/client';
 
 export function useContacts() {
-  const result = useQuery(['contacts'], async () => {
-    return await client('https://jsonplaceholder.typicode.com/users');
-  });
+  const result = useQuery(
+    ['contacts'],
+    async () => {
+      return await client('https://jsonplaceholder.typicode.com/users');
+    },
+    { refetchOnWindowFocus: false }
+  );
 
   return result;
 }
