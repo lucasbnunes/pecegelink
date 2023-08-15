e0d0c24d2640930116f6f41ccf07dca146c29dfb
lucasbnunes <lucasbnunes23@gmail.com>
2023-08-15 20:18:23 -0300

    chore: upgrade react-query to tanstack-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { theme } from './styles/theme'
 import { GlobalStyle } from './styles/global'
 import { Home } from './pages/home'
 import { Layout } from './components/Layout'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Layout>
--- src/features/contacts/useContacts.ts
+++ src/features/contacts/useContacts.ts
@@ -1,8 +1,8 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { client } from '../../api/client';
 
 export function useContacts() {
-  const result = useQuery('contacts', async () => {
+  const result = useQuery(['contacts'], async () => {
     return await client('https://jsonplaceholder.typicode.com/users');
   });
 
