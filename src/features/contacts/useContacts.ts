1aaaf978ef253a1284d86615ce1fdba80e6dc6c6
lucasbnunes <lucasbnunes23@gmail.com>
2023-08-17 13:07:54 -0300

    fix: remove unnecessary async await

--- src/features/contacts/useContacts.ts
+++ src/features/contacts/useContacts.ts
@@ -4,8 +4,8 @@ import { client } from '../../api/client';
 export function useContacts() {
   const result = useQuery(
     ['contacts'],
-    async () => {
-      return await client('https://jsonplaceholder.typicode.com/users');
+    () => {
+      return client('https://jsonplaceholder.typicode.com/users');
     },
     { refetchOnWindowFocus: false }
   );
